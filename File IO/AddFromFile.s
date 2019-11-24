4d26123e6ee96bfcbae66151d960fcdcfe84d121
rbradt <rbradt1@saddleback.edu>
2019-11-24 02:22:20 -0800

    update getch and getline

--- File IO/AddFromFile.s
+++ File IO/AddFromFile.s
@@ -15,14 +15,14 @@
 	.data
 buff:		.skip 1024
 	
+	.text
 AddFromFile:
 	push {r1-r11, lr}
-
 
 	/* -------------- Open File -------------- */
 
 	ldr r0, =infileName	@ r0 has infileName
-	mov r1, #00		@ Flag - Can write/create/truncate
+	mov r1, #00			@ Flag - Can read
 	mov r2, #0644		@ Mode - I can read/write, others read
 
 	mov r7, #5			@ Open file (returns fileName)
@@ -30,11 +30,7 @@ AddFromFile:
 	
 loop:
 	ldr r1, =buff
-	mov r4, #0
-	str r4, [r1]
-
 	mov r2, r0
-	
 	bl getline
 	
 	cmp r0, #0
--- File IO/getline.s
+++ File IO/getline.s
@@ -1,64 +1,78 @@
-/* -------------------------------------------
-	Function:	getline
-	Class:		CS3B
-	Purpose:	get line from file
-	------------------------------------------
-	Parameters needed:
-		R2:	File handle
-		R1:	Buffer
-	------------------------------------------
-	Preconditions:
-		File exists & is open for reading.
-	------------------------------------------
-	Returns:
-		R0: Bytes read
-		R3: Copied string
-	------------------------------------------*/
-
+	.extern malloc
 	.global getline
-	
+/**************************************************************************
+ * 								getline
+ *
+ *		This function creates a new string with a single line of characters
+ *	from the open file passed.
+ *-------------------------------------------------------------------------
+ * input:
+ *	r1 = buffer
+ *	r2 = file handle
+ * 
+ * output:
+ *	r0 = string
+ *	r1 = bytes allocated
+ *-------------------------------------------------------------------------
+ * note:
+ *	the number of bytes read will be zero if at the end of the file
+ **************************************************************************/
 getline:
-	push {r4-r8, r10, r11, lr}
+	push {r2-r11, lr}
+	mov r3, #0			@ clear buffer
+	str r3, [r1]
 	
-	mov r6, #0
-	mov r4, #0
-	str r4, [r1]
 	
-	mov r4, r1			@ r4 has buffer
-	mov r5, #-1			@ Counter
+	/* load characters from file into the buffer */
+	push {r1}			@ save initial buffer location
+	mov r5, #0			@ number of characters
+	loadBuffer:
+		bl getch		@ load character into buffer
+		ldrb r4, [r1]
+		
+		add r5, r5, r0		@ increment character count (count += bytes_read)
+		
+		cmp r0, #0		@ if: 1. no bytes are read
+		beq createString
+		cmp r4, #0xa		@     2. '\n' is read
+		beq createString
+		cmp r4, #0xd		@     3. '\r' is read
+		beq createString	@ the end of the line has been reached
+		
+		add r1, r1, #1		@ shift buffer
+		
+		b loadBuffer
+		
+		
+	/* create a new string with the characters from the file */
+	createString:
+		pop {r1}		@ pop buffer location
+		
+		cmp r5, #0		@ if no bytes were read -> exit
+		beq return
+		
+		push {r1-r11}		@ allocate memory for new string
+		mov r0, r5
+		add r0, #1
+		bl malloc
+		pop {r1-r11}
+		
+		mov r3, #-1		@ copy characters into new string
+		copy:
+			add r3, r3, #1
+			
+			ldrb r4, [r1, r3]
+			strb r4, [r0, r3]
+			
+			cmp r3, r5
+			blt copy
 
-loop:
-	add r5, #1			@ Update counter
-	mov r0, r2
-	bl getch
-	cmp r0, #0			@ if r0 != 0
-	beq copy
-
-	cmp r1, #0xa			@ if ch = \n
-	beq lineNew
-	cmp r1, #0xd			@ if ch = \r
-	beq lineNew
-	
-	strb r1, [r4, r5]	@ Put the char into buffer
-	
-	b loop
-	
-lineNew:
-	mov r6, r1
-	mov r1, #'a'
-	strb r1, [r4, r5]
-	
-copy:
-	mov r1, r4	
-	bl String_copy
-	mov r3, r0
-	
-	cmp r6, #0
-	beq end
-	
-	strb r6, [r3, r5]
-
-end:
-	pop {r4-r8, r10, r11, lr}
+		mov r4, #0		@ add null at end of string
+		strb r4, [r0, r5]
+		add r5, r5, #1
+			
+return:
+	mov r1, r5
+	pop {r2-r11, lr}
 	bx lr
 	.end
