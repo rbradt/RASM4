1a0b103a9a8a4b7dcf762c01732b85919be1374f
jay <44626277+spacefilms@users.noreply.github.com>
2019-11-26 09:06:58 -0800

    Update ViewStrings.s

--- Linked List/ViewStrings.s
+++ Linked List/ViewStrings.s
@@ -5,7 +5,6 @@
 	------------------------------------------
 	Parameters needed:
 		R1: head ptr
-		R2: tail ptr
 		R3: # of nodes
 	------------------------------------------
 	Returns:
@@ -13,22 +12,49 @@
 	------------------------------------------*/
 	
 	.global ViewStrings
+	.data
+temp:		.word	0
+emptyErr:	.asciz "\n*** ERROR - NO STRINGS IN LIST ***\nEXITING...\n"
+endl:		.byte	10
 	
 	.text
 ViewStrings:
 	push {r1-r12, lr}
 	
-	mov r4, #-1			@ Counter
+	cmp r1, #0
+	beq emptyList
+	
+	ldr r1, [r1]		@ Dereference head ptr
+	ldr r2, =temp
+	str r1, [r2]		@ head -> temp
+	
+	mov r4, #-1		@ Counter
 loop:	
 	add r4, #1
-
-	ldr r1, [r1, #4]		@ Traverse thru linked list
-	bl putstring			@ Print string in node
+	
+	ldr r5, [r2]		@ Deref temp
+	cmp r5, #0
+	beq endLoop
+	ldr r1, [r5]
+	bl putstring
+	
+	ldr r1, =endl
+	bl putch
+	
+	ldr r6, [r5, #4]
+	ldr r2, =temp
+	str r6, [r2]
 	
 	cmp r4, r3
-	blt loop			@ Loop if less than
+	blt loop		@ Loop if less than
 	
 endLoop:
 	pop {r1-r12, lr}
 	bx lr
+	
+emptyList:
+	ldr r1, =emptyErr
+	bl putstring
+	b endLoop
+	
   .end
