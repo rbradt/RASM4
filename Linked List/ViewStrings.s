1ff237d5b765cb68b062f5661b55645998265a08
jay <44626277+spacefilms@users.noreply.github.com>
2019-11-25 23:03:42 -0800

    Create ViewStrings.s

---
+++ Linked List/ViewStrings.s
@@ -0,0 +1,22 @@
+/* -------------------------------------------
+	Function:	ViewStrings
+	Class:		CS3B
+	Purpose:	Output all strings in linked list.
+	------------------------------------------
+	Parameters needed:
+		
+	------------------------------------------*/
+	
+	.global ViewStrings
+	
+	.data
+	
+ViewStrings:
+	push {r1-r12, lr}
+	
+
+
+	
+	pop {r1-r12, lr}
+	bx lr
+  .end
