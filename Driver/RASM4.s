fb35a074c9621ecfa5f824b75871762831f64793
jay <44626277+spacefilms@users.noreply.github.com>
2019-11-26 11:30:59 -0800

    Update RASM4.s

--- Driver/RASM4.s
+++ Driver/RASM4.s
@@ -32,9 +32,9 @@ buffer:			.space 1024
 	.balign 4
 	
 _start:
-	mov r2, #0			@ Init memory consumption
-	mov r3, #0			@ Init num of nodes
-	ldr r8, =iInputInt	@ .word, where input # will be stored
+	ldr r8, =iInputInt		@ .word, where input # will be stored
+	mov r1, #0
+	bl llInsert			@ Create linked list
 
 /* -------------- Begin Menu Loop While Input != '7' -------------- */
 whileNotQuit:
@@ -46,14 +46,14 @@ whileNotQuit:
 	ldr r3, [r3]
 	bl Menu				@ Outputs menu - need mem consump in r2, # of nodes in r3
 
-	ldr r1, =strInput	@ Prompt user for input
+	ldr r1, =strInput		@ Prompt user for input
 	bl putstring
 	ldr r1, =iInputVal
 	ldr r2, =BUFSIZE
-	bl getstring		@ Get input from user
+	bl getstring			@ Get input from user
 	bl ascint32			@ Convert input to number
 	@ldr r1, =iInputInt
-	@str r0, [r1]		@ Store into word (iInputInt)
+	@str r0, [r1]			@ Store into word (iInputInt)
 	@mov r8, r1
 	
 switch:
@@ -75,8 +75,6 @@ switch:
 
 
 ifChoice1:				@ If user chooses to view all strings
-	bl CLS
-	
 	ldr r1, =head
 	ldr r1, [r1]
 	ldr r3, =iNodes
@@ -84,23 +82,18 @@ ifChoice1:				@ If user chooses to view all strings
 	
 	bl ViewStrings
 	
-	ldr r5, =head		@ save linked list
-	str r1, [r5]
-	ldr r5, =tail
-	str r2, [r5]
+	b whileNotQuit			@ Go back to menu
 	
-	b whileNotQuit		@ Go back to menu
-	
-ifChoice2:			@ If user chooses to add string
+ifChoice2:				@ If user chooses to add string
 	ldr r1, =strChoice2
 	bl putstring
-	ldr r1, =strInput	@ Prompt user for 2nd input (a or b)
+	ldr r1, =strInput		@ Prompt user for 2nd input (a or b)
 	bl putstring
 	ldr r1, =iInputVal
 	ldr r2, =BUFSIZE
-	bl getstring		@ Get input from user
+	bl getstring			@ Get input from user
 
-	ldrb r1, [r1]
+	ldrb r1, [r1]			@ Dereference iInputVal
 
 	cmp r1, #'a'
 	beq fromKeyb
@@ -119,12 +112,22 @@ fromKeyb:				@ If user chooses to add string from keyboard
 	
 	bl AddFromKeyb
 	
-	ldr r5, =head		@ save linked list
+	ldr r5, =head			@ save linked list
 	str r1, [r5]
 	ldr r5, =tail
 	str r2, [r5]
 	
-	b whileNotQuit		@ Go back to menu
+	ldr r5, =iMemory		@ update allocated memory quantity
+	ldr r6, [r5]
+	add r6, r6, r0
+	str r6, [r5]
+	
+	ldr r5, =iNodes			@ update node counter
+	ldr r6, [r5]
+	add r6, r6, r3
+	str r6, [r5]
+	
+	b whileNotQuit			@ Go back to menu
 	
 fromFile:				@ If user chooses to add string from file
 	ldr r1, =head
@@ -132,27 +135,26 @@ fromFile:				@ If user chooses to add string from file
 	ldr r2, =tail
 	ldr r2, [r2]
 	ldr r3, =infileName
-	bl AddFromFile		@ Returns in r3 # of nodes added to the linked list
+	bl AddFromFile			@ Returns in r3 # of nodes added to the linked list
 	
-	ldr r5, =head		@ save linked list
+	ldr r5, =head			@ save linked list
 	str r1, [r5]
 	ldr r5, =tail
 	str r2, [r5]
 	
-	ldr r5, =iMemory	@ update allocated memory quantity
+	ldr r5, =iMemory		@ update allocated memory quantity
 	ldr r6, [r5]
 	add r6, r6, r0
 	str r6, [r5]
 	
-	ldr r5, =iNodes		@ update node counter
+	ldr r5, =iNodes			@ update node counter
 	ldr r6, [r5]
 	add r6, r6, r3
 	str r6, [r5]
 	
-	b whileNotQuit		@ Go back to menu
+	b whileNotQuit			@ Go back to menu
 	
 ifChoice3:				@ If user chooses to delete string
-	bl CLS
 	ldr r1, =strIndex
 	bl putstring
 	ldr r1, =inputIndex
@@ -161,12 +163,12 @@ ifChoice3:				@ If user chooses to delete string
 	ldr r1, =inputIndex
 	bl ascint32			@ Change into int
 	ldr r5, =iInputInt
-	str r0, [r5]		@ Store index into r5
+	str r0, [r5]			@ Store index into r5
 	
 	ldr r1, =endl
 	bl putch
 	
-	ldr r3, [r5]		@ Move index to r3 to pass into func
+	ldr r3, [r5]			@ Move index to r3 to pass into func
 	mov r5, #0			@ Clear r5
 	ldr r1, =head
 	ldr r1, [r1]
@@ -174,7 +176,7 @@ ifChoice3:				@ If user chooses to delete string
 	ldr r2, [r2]
 	bl llRemoveAtIndex
 	
-	ldr r5, =head		@ save linked list
+	ldr r5, =head			@ save linked list
 	str r1, [r5]
 	ldr r5, =tail
 	str r2, [r5]
@@ -187,23 +189,22 @@ ifChoice3:				@ If user chooses to delete string
 	sub r6, r6, r0
 	str r6, [r5]
 	
-	ldr r5, =iNodes		@ update node counter
+	ldr r5, =iNodes			@ update node counter
 	ldr r6, [r5]
 	sub r6, r6, #1
 	str r6, [r5]
 	
-	push {r1-r11}		@ free string
+	push {r1-r11}			@ free string
 	mov r0, r3	
 	bl free
 	pop {r1-r11}
 	
 	ldr r1, =strDeleted
-	bl putstring		@ Output confirmation
+	bl putstring			@ Output confirmation
 	
 	b whileNotQuit
 
 ifChoice4:				@ If user chooses to edit string
-	bl CLS
 	ldr r1, =strIndex
 	bl putstring
 	ldr r1, =inputIndex
@@ -212,45 +213,45 @@ ifChoice4:				@ If user chooses to edit string
 	ldr r1, =inputIndex
 	bl ascint32			@ Change into int
 	ldr r5, =iInputInt
-	str r0, [r5]		@ Store index into r5
+	str r0, [r5]			@ Store index into r5
 	
-	ldr r1, =head		@ edit string
+	ldr r1, =head			@ edit string
 	ldr r1, [r1]
 	ldr r2, =tail
 	ldr r2, [r2]
 	ldr r3, [r5]
 	bl editString
 	
-	ldr r5, =iMemory	@ update allocated memory quantity
+	ldr r5, =iMemory		@ update allocated memory quantity
 	ldr r6, [r5]
 	add r6, r6, r0
 	str r6, [r5]
 	
-	b whileNotQuit		@ Go back to menu
+	b whileNotQuit			@ Go back to menu
 	
 ifChoice5:				@ If user chooses to search for a string
-	bl CLS
 	ldr r1, =strSubstring
 	bl putstring
 	ldr r1, =buffer
 	ldr r2, =1024
 	bl getstring
 
-	ldr r1, =head		@ search for string
+	ldr r1, =head			@ search for string
 	ldr r1, [r1]
 	ldr r2, =tail
 	ldr r2, [r2]
 	ldr r3, =buffer
 	bl editString
 	
-	b whileNotQuit		@ Go back to menu
+	b whileNotQuit			@ Go back to menu
 	
 ifChoice6:				@ If user chooses to save file
-	bl CLS
 	bl SaveFile			@ # of nodes must be in r3
+	ldr r1, =endl
+	bl putch
 	ldr r1, =strSaved
 	bl putstring
-	b whileNotQuit		@ Go back to menu
+	b whileNotQuit			@ Go back to menu
 
 
 end:
