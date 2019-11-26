b1613e1f5bc169ee3ab01d710ab9f7b319d4982b
jay <44626277+spacefilms@users.noreply.github.com>
2019-11-26 11:33:54 -0800

    Update SaveFile.s

--- File IO/SaveFile.s
+++ File IO/SaveFile.s
@@ -15,11 +15,16 @@
  temp:		.word	0
  endl:		.byte	10
   
-  
+	.text
+	.balign 4
   SaveFile:
  	   push {r1-r11, lr}
+	   
+	   cmp r3, #0		@ If there are no strings, exit
+	   beq endLoop
 		
 	   ldr r1, [r1]		@ Dereference head ptr
+	   ldr r5, =temp
 	   str r1, [r5]		@ Put head in r5
     
     /* -------------- Open File -------------- */
@@ -31,32 +36,32 @@
   	mov r7, #5		@ Open file (returns fileName)
   	svc 0			@ Supervisor call
 	
-	ldr r6, =temp
-	str r5, [r6]		@ head -> temp
-	
 	mov r4, #-1		@ Counter
   loop:	
   	add r4, #1
 	
-	ldr r8, [r6]		@ Deref temp
-
+	ldr r6, [r5]		@ Deref temp
+	
+	ldr r1, [r6]		@ R1: Memory
+	bl String_length
+	mov r2, r0		@ R2: Length
+	
+	ldr r0, =outfile	@ R0: fileName
    	mov r7, #4		@ Write to file
   	svc 0            	@ Supervisor call
 	
-	ldr r1, =endl
-	bl putch
-	
-	ldr r9, [r8, #4]
-	ldr r6, =temp
-	str r9, [r6]
+	ldr r8, [r6, #4]
+	ldr r5, =temp
+	str r8, [r5]
 	
 	cmp r4, r3		@ Compare counter to # of strings in file
 	blt loop		@ Loop again if less than
 	
-  endLoop:
+  closeFile:
   	mov r7, #6		@ Close file
   	svc 0
-    
+	
+  endLoop:
  	pop {r1-r11, lr}
 	bx lr
  	.end
