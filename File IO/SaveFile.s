df45c594c51e231bcab467c20a9751128877c3a9
jay <44626277+spacefilms@users.noreply.github.com>
2019-11-26 09:35:42 -0800

    Update SaveFile.s

--- File IO/SaveFile.s
+++ File IO/SaveFile.s
@@ -5,7 +5,6 @@
 	------------------------------------------
 	Parameters needed:
     		R1: head ptr
-		R2: tail ptr
 		R3: # of strings in file
 	------------------------------------------*/
   
@@ -13,32 +12,43 @@
 	
 	.data
  outfile:	.asciz	"output.txt"
+ temp:		.word	0
  endl:		.byte	10
   
   
   SaveFile:
  	   push {r1-r11, lr}
-	   
-	   str r8, [r1]		@ Put head in r8
-	   str r9, [r2]		@ Put tail in r9
+		
+	   ldr r1, [r1]		@ Dereference head ptr
+	   str r1, [r5]		@ Put head in r5
     
     /* -------------- Open File -------------- */
 
   	ldr r0, =outfile  	@ r0 has outfile
-  	mov r1, #01101     	@ Flag - Can write/create/truncate
+  	mov r1, #0101     	@ Flag - Can write/create
   	mov r2, #0644	    	@ Mode - I can read/write, others read
 
   	mov r7, #5		@ Open file (returns fileName)
   	svc 0			@ Supervisor call
 	
+	ldr r6, =temp
+	str r5, [r6]		@ head -> temp
+	
 	mov r4, #-1		@ Counter
   loop:	
   	add r4, #1
 	
-	ldr r8, [r8, #4]	@ Traverse thru ll
+	ldr r8, [r6]		@ Deref temp
 
    	mov r7, #4		@ Write to file
   	svc 0            	@ Supervisor call
+	
+	ldr r1, =endl
+	bl putch
+	
+	ldr r9, [r8, #4]
+	ldr r6, =temp
+	str r9, [r6]
 	
 	cmp r4, r3		@ Compare counter to # of strings in file
 	blt loop		@ Loop again if less than
