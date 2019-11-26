97213be6b9522272c0bd84a1991a6935aacc6467
jay <44626277+spacefilms@users.noreply.github.com>
2019-11-26 01:13:33 -0800

    Update ViewStrings.s

--- Linked List/ViewStrings.s
+++ Linked List/ViewStrings.s
@@ -20,13 +20,12 @@ ViewStrings:
 	push {r1-r12, lr}
 	
 	mov r4, #-1			@ Counter
-loop:					@ Traverse thru linked list
+loop:	
 	add r4, #1
 
-
+	ldr r1, [r1, #4]		@ Traverse thru linked list
+	bl putstring			@ Print string in node
 	
-
-
 	cmp r4, r3
 	blt loop			@ Loop if less than
 	
