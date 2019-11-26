10a95dac4df41b04f8814cc770b6078b5b690d05
rbradt <rbradt1@saddleback.edu>
2019-11-26 12:10:47 -0800

    updated add from keyboard

--- Driver/AddFromKeyb.s
+++ Driver/AddFromKeyb.s
@@ -41,24 +41,27 @@ AddFromKeyb:
 	ldr r2, =BUFSIZE
 	bl getstring
 	
-	ldr r3, =szInput
-	ldr r1, =iInput
-	str r1, [r3]		@ Store into r3 the data
+	bl String_copy
+	mov r3, r0
 	
 	ldr r1, =head
 	ldr r1, [r1]
 	ldr r2, =tail
 	ldr r2, [r2]
 	bl llInsert		@ Insert node into linked list
-	add r6, #1		@ Keep track of # of nodes
-
-	ldr r5, =head		@ store new head and tail
+	
+	ldr r5, =head	@ store new head and tail
 	str r1, [r5]
 	ldr r5, =tail
 	str r2, [r5]
+	
+	add r6, #1		@ Keep track of # of nodes
 
-	add r4, #8		@ keep track of data allocated
-	add r4, r4, r1
+	ldr r1, =szInput
+	bl String_length
+	
+	add r4, #9		@ keep track of data allocated (+1 for null since strings are asciz)
+	add r4, r4, r0
 	
 	mov r0, r6		@ R0: # of nodes
 	mov r3, r4		@ R3: data allocated
